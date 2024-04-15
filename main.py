aa87d5163b944ce2006aae494b0c050000ed44d6
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-04-15 14:21:14 +0100

    feat: add main.py as entry point of the app

---
+++ main.py
@@ -0,0 +1,54 @@
+"""main module"""
+import flet as ft
+from task import Task
+
+class TodoApp(ft.Column):
+    """UI components"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.new_task = None
+        self.tasks = None
+
+    def build(self):
+        """build method"""
+        super().build()
+
+        self.new_task = ft.TextField(
+            hint_text="Whats needs to be done?", expand=True)
+        self.tasks = ft.Column()
+
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_task,
+                        ft.FloatingActionButton(
+                            icon=ft.icons.ADD, on_click=self.add_clicked),
+                    ],
+                ),
+                self.tasks,
+            ],
+        )
+
+    def add_clicked(self, _):
+        """add clicked method"""
+        task = Task(self.new_task.value)
+        self.tasks.controls.append(task)
+        self.new_task.value = ""
+        self.update()
+
+
+if __name__ == "__main__":
+    def main(page: ft.Page):
+        """entry function"""
+        page.title = "ToDo App"
+        page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+        page.update()
+
+        todo1 = TodoApp()
+        todo2 = TodoApp()
+
+        page.add(todo1, todo2)
+
+    ft.app(target=main)

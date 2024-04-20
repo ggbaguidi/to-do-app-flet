1e551f1e89e3280fbc36d76f8b714ea0b949aba8
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-04-20 21:14:11 +0100

    feat: add filter and view functions

--- main.py
+++ main.py
@@ -7,15 +7,41 @@ class TodoApp(ft.Column):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.new_task = None
+        self.filter = None
+        self.view = None
         self.tasks = None
 
     def build(self):
         """build method"""
         super().build()
 
-        self.new_task = ft.TextField(
-            hint_text="Whats needs to be done?", expand=True)
+        self.new_task = ft.TextField(hint_text="Whats needs to be done?", expand=True)
         self.tasks = ft.Column()
+
+        self.filter = ft.Tabs(
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[ft.Tab(text="all"), ft.Tab(text="active"), ft.Tab(text="completed")],
+        )
+
+        self.view = ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_task,
+                        ft.FloatingActionButton(icon=ft.icons.ADD, on_click=self.add_clicked),
+                    ],
+                ),
+                ft.Column(
+                    spacing=25,
+                    controls=[
+                        self.filter,
+                        self.tasks,
+                    ],
+                ),
+            ],
+        )
 
         return ft.Column(
             width=600,
@@ -23,18 +49,23 @@ class TodoApp(ft.Column):
                 ft.Row(
                     controls=[
                         self.new_task,
-                        ft.FloatingActionButton(
-                            icon=ft.icons.ADD, on_click=self.add_clicked),
+                        ft.FloatingActionButton(icon=ft.icons.ADD, on_click=self.add_clicked),
                     ],
                 ),
-                self.tasks,
+                ft.Column(
+                    spacing=25,
+                    controls=[
+                        self.filter,
+                        self.tasks,
+                    ],
+                ),
             ],
         )
 
     def add_clicked(self, _):
         """add clicked method"""
         if self.new_task.value:
-            task = Task(self.new_task.value, self.delete_clicked)
+            task = Task(self.new_task.value, self.task_status_change, self.delete_clicked)
             self.tasks.controls.append(task)
         self.new_task.value = ""
         self.update()
@@ -42,6 +73,25 @@ class TodoApp(ft.Column):
     def delete_clicked(self, task):
         """delete ckicked method"""
         self.tasks.controls.remove(task)
+        self.update()
+
+    def update(self):
+        """update"""
+        status = self.filter.tabs[self.filter.selected_index].text
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "all"
+                or (status == "active" and not task.completed)
+                or (status == "completed" and task.completed)
+            )
+        super().update()
+
+    def tabs_changed(self, _):
+        """tabs changed"""
+        self.update()
+
+    def task_status_change(self, _):
+        """change the task status"""
         self.update()
 
 
